f076c01be0e979244c72b55feef36b9897c595db
Maciej Loj <maciej.loj@gmail.com>
2019-11-05 07:59:25 +0100

    Contact page updated

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,140 +1,53 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.new_app')
 
-  <head>
-    <title>Realtors &mdash; Website Template by Colorlib</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link rel="stylesheet" href="css/template/bootstrap.min.css">
-    <link rel="stylesheet" href="css/template/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/template/jquery.fancybox.min.css">
-    <link rel="stylesheet" href="css/template/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/template/owl.theme.default.min.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/template/aos.css">
-
-    <!-- MAIN CSS -->
-    <link rel="stylesheet" href="css/template/style.css">
-
-  </head>
-
-  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-
-
-    <div class="site-wrap" id="home-section">
-
-      <div class="site-mobile-menu site-navbar-target">
-        <div class="site-mobile-menu-header">
-          <div class="site-mobile-menu-close mt-3">
-            <span class="icon-close2 js-menu-toggle"></span>
-          </div>
-        </div>
-        <div class="site-mobile-menu-body"></div>
-      </div>
-
-
-
-      <header class="site-navbar site-navbar-target" role="banner">
-
-        <div class="container">
-          <div class="row align-items-center position-relative">
-
-            <div class="col-3 ">
-              <div class="site-logo">
-                {{-- <a href="index.html">Realtors</a> --}}
-              </div>
-            </div>
-
-            <div class="col-9  text-right">
-
-
-              <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
-
-
-
-              <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
-                <ul class="site-menu main-menu js-clone-nav ml-auto ">
-                    <li><a href="/" class="nav-link">Strona główna</a></li>
-                    <li><a href="/ogloszenia" class="nav-link">Ogłoszenia</a></li>
-                    <li class="active"><a href="contact.html" class="nav-link">Contact</a></li>
-                </ul>
-              </nav>
-            </div>
-
-
-          </div>
-        </div>
-
-      </header>
-
-    <div class="ftco-blocks-cover-1">
-      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_1.jpg')">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
-            <div class="col-md-7">
-              <h1 class="mb-2">Skontaktuj się z nami</h1>
-              <p class="text-white">.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-
+@section('content')
 
     <div class="site-section bg-light" id="contact-section">
       <div class="container">
-        <div class="row justify-content-center text-center">
-        <div class="col-7 text-center mb-5">
-          <h2>.</h2>
-          <p>.</p>
-        </div>
-      </div>
         <div class="row">
           <div class="col-lg-8 mb-5" >
+
+            {{-- Do dodania formularz - Laravel Collective --}}
+
             <form action="#" method="post">
               <div class="form-group row">
                 <div class="col-md-6 mb-4 mb-lg-0">
-                  <input type="text" class="form-control" placeholder="First name">
+                  <input type="text" class="form-control" placeholder="Imię">
                 </div>
                 <div class="col-md-6">
-                  <input type="text" class="form-control" placeholder="First name">
+                  <input type="text" class="form-control" placeholder="Nazwisko">
                 </div>
               </div>
 
               <div class="form-group row">
                 <div class="col-md-12">
-                  <input type="text" class="form-control" placeholder="Email address">
+                  <input type="text" class="form-control" placeholder="Email">
                 </div>
               </div>
 
               <div class="form-group row">
                 <div class="col-md-12">
-                  <textarea name="" id="" class="form-control" placeholder="Write your message." cols="30" rows="10"></textarea>
+                  <textarea name="" id="" class="form-control" placeholder="Twoja wiadomość" cols="30" rows="10"></textarea>
                 </div>
               </div>
+
               <div class="form-group row">
                 <div class="col-md-6 mr-auto">
-                  <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Send Message">
+                  <input type="submit" class="btn btn-block btn-primary text-white py-3 px-5" value="Wyślij">
                 </div>
               </div>
+
             </form>
           </div>
           <div class="col-lg-4 ml-auto">
             <div class="bg-white p-3 p-md-5">
-              <h3 class="text-black mb-4">Contact Info</h3>
+              <h3 class="text-black mb-4">Kontakt</h3>
               <ul class="list-unstyled footer-link">
                 <li class="d-block mb-3">
-                  <span class="d-block text-black">Address:</span>
-                  <span>34 Street Name, City Name Here, United States</span></li>
-                <li class="d-block mb-3"><span class="d-block text-black">Phone:</span><span>+1 242 4942 290</span></li>
-                <li class="d-block mb-3"><span class="d-block text-black">Email:</span><span>info@yourdomain.com</span></li>
+                  <span class="d-block text-black">Adres:</span>
+                  <span> - </span></li>
+                <li class="d-block mb-3"><span class="d-block text-black">Telefon:</span><span> - </span></li>
+                <li class="d-block mb-3"><span class="d-block text-black">Email:</span><span> - </span></li>
               </ul>
             </div>
           </div>
@@ -142,67 +55,4 @@
       </div>
     </div>
 
-
-
-
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <h3 class="text-white h5 mb-3">Subscribe</h3>
-            <form action="" class="d-flex">
-              <input type="text" class="form-control mr-3" placeholder="Enter your email">
-              <input type="submit" class="btn btn-primary text-white" value="Send Now">
-            </form>
-          </div>
-          <div class="col-md-3 ml-auto">
-            <h3 class="text-white h5 mb-3">Subscribe</h3>
-            <ul class="list-unstyled menu-arrow">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Testimonials</a></li>
-              <li><a href="#">Terms of Service</a></li>
-              <li><a href="#">Privacy</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <h3 class="text-white h5 mb-3">About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut dolores deserunt, obcaecati fuga quo. Autem explicabo sapiente, maiores.</p>
-          </div>
-        </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <div class="border-top pt-5">
-              <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </footer>
-
-    </div>
-
-    <script src="js/template/jquery-3.3.1.min.js"></script>
-    <script src="js/template/jquery-migrate-3.0.0.js"></script>
-    <script src="js/template/popper.min.js"></script>
-    <script src="js/template/bootstrap.min.js"></script>
-    <script src="js/template/owl.carousel.min.js"></script>
-    <script src="js/template/jquery.sticky.js"></script>
-    <script src="js/template/jquery.waypoints.min.js"></script>
-    <script src="js/template/jquery.animateNumber.min.js"></script>
-    <script src="js/template/jquery.fancybox.min.js"></script>
-    <script src="js/template/jquery.stellar.min.js"></script>
-    <script src="js/template/jquery.easing.1.3.js"></script>
-    <script src="js/template/bootstrap-datepicker.min.js"></script>
-    <script src="js/template/aos.js"></script>
-
-    <script src="js/template/main.js"></script>
-
-  </body>
-
-</html>
+@endsection
